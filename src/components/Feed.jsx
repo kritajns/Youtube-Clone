23804cbafb76fe20a83ff57d951221fe3d8c8aff
kritajns <kritajnshrest@gmail.com>
2023-06-14 08:16:22 +0545

    category & videos added

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,11 +1,36 @@
 import React, {useState, useEffect} from 'react';
 import { Box, Stack, Typography } from '@mui/material';
+import { SideBar, Videos } from './';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState('New');
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`);
+  }, [selectedCategory]);
+
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row"}}}>
-      <Box>
-        
+      <Box sx={{ height: { sx: 'auto', md: '92vh' }, 
+      borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2 }}}>
+        <SideBar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
+
+        <Typography className='copyright' variant='body2'
+          sx={{ mt: 1.5, color: '#fff' }}>
+          Copyright JSM Media 2023
+        </Typography>
+      </Box>
+
+      <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2}}>
+        <Typography variant='h4' fontWeight="bold" mb={2} sx={{ color: 'white'}}>
+          {selectedCategory} <span style={{ color: '#F31503'}}>Videos</span>
+        </Typography>
+
+        <Videos videos={[]} />
       </Box>
     </Stack>
   )
