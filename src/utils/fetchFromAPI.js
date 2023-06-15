52cbf7f4b1f723466af32dfc6db6ffec3c48598e
kritajns <kritajnshrest@gmail.com>
2023-06-15 08:26:55 +0545

    updated api key

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = 'https://youtube-v31.p.rapidapi.com/';
+const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
 
 const options = {
   url: BASE_URL,
@@ -8,7 +8,8 @@ const options = {
     maxResults: '50'
   },
   headers: {
-    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    // 'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Key': 'ad479d7f42msh64aef1aabacf1fep1db671jsnb05340fb130e',
     'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
   }
 };
